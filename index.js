5ebec3a6726901b4f8e1b48492b5030950ef1d5b
osha7 <ogroetz@gmail.com>
2020-09-26 08:04:55 -0600

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
  
 function largestSubarraySum(array){
     let curr_max = 0, max_so_far = 0
-
+    
+// Math.max returns the largest of the zero or more numbers given as input parameters
+// Math.max([value1[, value2[, ...]]])
+// parameter: value1, value2, ... ((((Numbers)))
+// return value: The largest of the given numbers. If at least one of the arguments cannot be converted to a number, NaN is returned.
     for(let i = 0; i < array.length; i++){
         curr_max = Math.max(0, curr_max + array[i]);
         max_so_far = Math.max(curr_max, max_so_far)
@@ -11,3 +15,5 @@ function largestSubarraySum(array){
 }
  
 // largestSubarraySum(array)
+
+// https://medium.com/@paulrohan/maximum-sum-of-subarray-a-javascript-implementaion-1cf512a58ea9
